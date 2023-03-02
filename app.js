ccf91be7a9932eb0772eafd6f1dfa63da0acf3bf
Dahalesahil <86490315+Dahalesahil@users.noreply.github.com>
2023-03-02 19:17:14 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./App.css";
+import Login from './components/Login';
+import Registration from "./components/Registration";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { Home } from "@mui/icons-material";
+import { useForm } from 'react-hook-form';
+
+
+
+function App() {
+  return (
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/Registration' element={<Registration />} />
+          <Route path='/Home' element={<Home />} />
+        </Routes>
+      </>
+    </Router>
+
+
+
+
+
+  )
+
+}
+
+export default App;
